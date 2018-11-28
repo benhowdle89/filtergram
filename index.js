32134cd6b6577e30be1dbaf1831f0c268b085976
Ben Howdle <benhowdle89@gmail.com>
2018-11-28 21:31:43 +0000

    req url

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ const requireHTTPS = (req, res, next) => {
     req.headers["x-forwarded-proto"] !== "https" &&
     process.env.NODE_ENV === "production"
   ) {
-    const secureUrl = `https://${req.headers["host"]}{req.url}`;
+    const secureUrl = `https://${req.headers["host"]}${req.url}`;
     res.writeHead(301, { Location: secureUrl });
     res.end();
   }
