f27939b54a5d4fc58442291189aa54d58b8325ce
Ben Howdle <benhowdle89@gmail.com>
2018-12-07 20:26:50 +0000

    redis

--- etc/cache.js
+++ etc/cache.js
@@ -1,3 +1,25 @@
-const cache = require("memory-cache");
+const { promisify } = require("util");
+const redis = require("redis");
+const REDIS_URL = process.env.REDIS_URL;
 
-module.exports = cache;
+const client = redis.createClient(REDIS_URL);
+
+const getAsync = promisify(client.get).bind(client);
+
+client.on("connect", () => {
+  console.log(`connected to redis`);
+});
+client.on("error", err => {
+  console.log(`Error: ${err}`);
+});
+
+module.exports = {
+  put(key, value, hours) {
+    client.setex(key, 60 * 60 * hours, JSON.stringify(value));
+  },
+  async get(key) {
+    const data = await getAsync(key);
+    if (data) return JSON.parse(data);
+    return null;
+  }
+};
