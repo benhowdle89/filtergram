51155584fb6a0f9f7524b16f5b7f6c1954052b2a
Ben Howdle <benhowdle89@gmail.com>
2019-04-15 09:41:49 +0100

    total endpoint, ig endpoint

--- api/index.js
+++ api/index.js
@@ -1,32 +1,32 @@
-const express = require("express");
-const bcrypt = require("bcryptjs");
-const asyncMiddleware = require("./../etc/async-middleware");
-const instagram = require("./../lib/instagram");
-const model = require("./../etc/model");
-const auth = require("./../etc/auth");
+const express = require('express')
+const bcrypt = require('bcryptjs')
+const asyncMiddleware = require('./../etc/async-middleware')
+const instagram = require('./../lib/instagram')
+const model = require('./../etc/model')
+const auth = require('./../etc/auth')
 
-const api = express.Router();
+const api = express.Router()
 
 const hashUserPassword = password => {
-  let hashed;
+  let hashed
   try {
-    hashed = bcrypt.hashSync(password, 10);
+    hashed = bcrypt.hashSync(password, 10)
   } catch (e) {
-    throw new Error("Error with password");
+    throw new Error('Error with password')
   }
-  return hashed;
-};
+  return hashed
+}
 
 const requireSession = async req => {
-  const { token } = req;
-  if (!token) return null;
+  const { token } = req
+  if (!token) return null
   try {
-    const session = await auth.fetch(token);
-    if (session) return session;
+    const session = await auth.fetch(token)
+    if (session) return session
   } catch (error) {
-    return null;
+    return null
   }
-};
+}
 
 const fetchInstagramProfilesForUsernames = async (
   usernames,
@@ -37,183 +37,201 @@ const fetchInstagramProfilesForUsernames = async (
       const media = await instagram.fetchInstagramProfile(
         u.username,
         !ignoreFilters && u.filters
-      );
-      if (!media.length) return null;
+      )
+      if (!media.length) return null
       return {
         media,
         username: u.username
-      };
+      }
     })
-  );
-  return items.filter(Boolean);
-};
+  )
+  return items.filter(Boolean)
+}
 
 api.get(
-  "/profiles/:userId",
+  '/users/total',
   asyncMiddleware(async (req, res) => {
-    const session = await requireSession(req);
+    const count = await model.getTotalUsers()
+    return res.json(count)
+  })
+)
+
+api.get(
+  '/ig/:instagramId',
+  asyncMiddleware(async (req, res) => {
+    const { instagramId } = req.params
+    if (!instagramId) return res.sendStatus(404)
+    const media = await instagram.fetchInstagramProfile(instagramId)
+    return res.json(media)
+  })
+)
+
+api.get(
+  '/profiles/:userId',
+  asyncMiddleware(async (req, res) => {
+    const session = await requireSession(req)
     if (!session) {
-      return res.sendStatus(401);
+      return res.sendStatus(401)
     }
-    const { userId } = req.params;
-    if (session.id != userId) return res.sendStatus(403);
-    const profiles = await model.getProfilesByUserId(userId);
+    const { userId } = req.params
+    if (session.id != userId) return res.sendStatus(403)
+    const profiles = await model.getProfilesByUserId(userId)
     if (!profiles.length) {
-      return res.json(profiles);
+      return res.json(profiles)
     }
     const medias = await fetchInstagramProfilesForUsernames(
       profiles.map(profile => {
         return {
           ...profile,
           filters: profile.filters ? profile.filters.filters : []
-        };
+        }
       })
-    );
+    )
     return res.json(
       profiles.map(profile => {
-        const userMedia = medias.find(m => m.username === profile.username);
+        const userMedia = medias.find(m => m.username === profile.username)
         return {
           ...profile,
           filters: profile.filters ? profile.filters.filters : [],
           media: userMedia ? userMedia.media : []
-        };
+        }
       })
-    );
+    )
   })
-);
+)
 
 api.post(
-  "/profiles/:userId",
+  '/profiles/:userId',
   asyncMiddleware(async (req, res) => {
-    const session = await requireSession(req);
+    const session = await requireSession(req)
     if (!session) {
-      return res.sendStatus(401);
+      return res.sendStatus(401)
     }
-    const { userId } = req.params;
-    if (session.id != userId) return res.sendStatus(403);
+    const { userId } = req.params
+    if (session.id != userId) return res.sendStatus(403)
 
-    const { usernames } = req.body;
+    const { usernames } = req.body
 
-    const medias = await fetchInstagramProfilesForUsernames(usernames, true);
+    const medias = await fetchInstagramProfilesForUsernames(usernames, true)
 
-    const validUsernames = medias.map(m => m.username);
+    const validUsernames = medias.map(m => m.username)
 
-    const currentUsernames = await model.getProfilesByUserId(userId);
+    const currentUsernames = await model.getProfilesByUserId(userId)
 
-    const toAdd = [];
-    const toUpdate = [];
+    const toAdd = []
+    const toUpdate = []
 
     const exists = username =>
-      currentUsernames.find(c => c.username === username);
+      currentUsernames.find(c => c.username === username)
 
     validUsernames.forEach(username => {
       if (exists(username)) {
-        toUpdate.push(username);
+        toUpdate.push(username)
       } else {
-        toAdd.push(username);
+        toAdd.push(username)
       }
-    });
+    })
 
     const toDelete = currentUsernames.filter(
       c => !validUsernames.includes(c.username)
-    );
+    )
 
     await Promise.all(
       toUpdate.map(username => {
-        const u = usernames.find(u => u.username === username);
+        const u = usernames.find(u => u.username === username)
         return model.updateProfileByUserId(userId, u.username, {
           filters: u.filters
-        });
+        })
       })
-    );
+    )
 
     await Promise.all(
       toAdd.map(username => {
-        const u = usernames.find(u => u.username === username);
+        const u = usernames.find(u => u.username === username)
         return model.addProfileByUserId(userId, u.username, {
           filters: u.filters
-        });
+        })
       })
-    );
+    )
 
     await Promise.all(
       toDelete.map(u => {
-        return model.removeProfileByUserId(userId, u.username);
+        return model.removeProfileByUserId(userId, u.username)
       })
-    );
+    )
 
     return res.json(
       medias.map(media => {
-        const { filters } = usernames.find(u => u.username === media.username);
+        const { filters } = usernames.find(u => u.username === media.username)
         return {
           ...media,
           filters
-        };
+        }
       })
-    );
+    )
   })
-);
+)
 
 api.get(
-  "/favourites/:userId",
+  '/favourites/:userId',
   asyncMiddleware(async (req, res) => {
-    const session = await requireSession(req);
+    const session = await requireSession(req)
     if (!session) {
-      return res.sendStatus(401);
+      return res.sendStatus(401)
     }
-    const { userId } = req.params;
-    if (session.id != userId) return res.sendStatus(403);
-    const favourites = await model.getFavouritesByUserId(userId);
-    return res.json(favourites);
+    const { userId } = req.params
+    if (session.id != userId) return res.sendStatus(403)
+    const favourites = await model.getFavouritesByUserId(userId)
+    return res.json(favourites)
   })
-);
+)
 
 api.post(
-  "/favourites/:userId",
+  '/favourites/:userId',
   asyncMiddleware(async (req, res) => {
-    const session = await requireSession(req);
+    const session = await requireSession(req)
     if (!session) {
-      return res.sendStatus(401);
+      return res.sendStatus(401)
     }
-    const { item } = req.body;
-    const { userId } = req.params;
-    if (session.id != userId) return res.sendStatus(403);
-    const favourite = await model.addFavouriteForUser(userId, item);
-    return res.json(favourite);
+    const { item } = req.body
+    const { userId } = req.params
+    if (session.id != userId) return res.sendStatus(403)
+    const favourite = await model.addFavouriteForUser(userId, item)
+    return res.json(favourite)
   })
-);
+)
 
 api.delete(
-  "/favourites/:userId/:instagramUrlId",
+  '/favourites/:userId/:instagramUrlId',
   asyncMiddleware(async (req, res) => {
-    const session = await requireSession(req);
+    const session = await requireSession(req)
     if (!session) {
-      return res.sendStatus(401);
+      return res.sendStatus(401)
     }
-    const { userId, instagramUrlId } = req.params;
-    if (session.id != userId) return res.sendStatus(403);
-    await model.removeFavouritesForUser(userId, instagramUrlId);
+    const { userId, instagramUrlId } = req.params
+    if (session.id != userId) return res.sendStatus(403)
+    await model.removeFavouritesForUser(userId, instagramUrlId)
     return res.json({
       instagram_url_id: instagramUrlId
-    });
+    })
   })
-);
+)
 
 api.post(
-  "/users/login",
+  '/users/login',
   asyncMiddleware(async (req, res) => {
-    let { email, password } = req.body;
+    let { email, password } = req.body
 
-    email = email.toLowerCase().trim();
-    password = password.trim();
-    const users = await model.getUserByEmail(email);
+    email = email.toLowerCase().trim()
+    password = password.trim()
+    const users = await model.getUserByEmail(email)
     if (!users.length) {
-      return res.sendStatus(404);
+      return res.sendStatus(404)
     }
-    const user = users[0];
-    const correctPassword = bcrypt.compareSync(password, user.password);
+    const user = users[0]
+    const correctPassword = bcrypt.compareSync(password, user.password)
     if (!correctPassword) {
-      return res.sendStatus(400);
+      return res.sendStatus(400)
     }
     return res.json({
       user: {
@@ -223,32 +241,32 @@ api.post(
       token: await auth.create({
         id: user.id
       })
-    });
+    })
   })
-);
+)
 
 api.post(
-  "/users",
+  '/users',
   asyncMiddleware(async (req, res) => {
-    let { email, password } = req.body;
-    email = email.toLowerCase().trim();
-    password = password.trim();
-    const users = await model.getUserByEmail(email);
+    let { email, password } = req.body
+    email = email.toLowerCase().trim()
+    password = password.trim()
+    const users = await model.getUserByEmail(email)
     if (users.length) {
-      return res.sendStatus(400);
+      return res.sendStatus(400)
     }
-    const hashedPassword = hashUserPassword(password.trim());
-    let insertedUsers;
+    const hashedPassword = hashUserPassword(password.trim())
+    let insertedUsers
     try {
-      insertedUsers = await model.createUser(email, hashedPassword);
+      insertedUsers = await model.createUser(email, hashedPassword)
     } catch (e) {
-      console.error("Error creating user", e);
-      return res.sendStatus(400);
+      console.error('Error creating user', e)
+      return res.sendStatus(400)
     }
     if (!insertedUsers.length) {
-      return res.sendStatus(400);
+      return res.sendStatus(400)
     }
-    const user = insertedUsers[0];
+    const user = insertedUsers[0]
     return res.json({
       user: {
         id: user.id,
@@ -257,8 +275,8 @@ api.post(
       token: await auth.create({
         id: user.id
       })
-    });
+    })
   })
-);
+)
 
-module.exports = api;
+module.exports = api
--- etc/model.js
+++ etc/model.js
@@ -1,73 +1,78 @@
-const query = require("./db");
+const query = require('./db')
+
+const getTotalUsers = async () => {
+  return query('SELECT COUNT(*) FROM users')
+}
 
 const getUserById = async id => {
-  return query(`SELECT * FROM users WHERE id = $1`, [id]);
-};
+  return query(`SELECT * FROM users WHERE id = $1`, [id])
+}
 
 const getUserByEmail = async email => {
-  return query(`SELECT * FROM users WHERE email = $1`, [email]);
-};
+  return query(`SELECT * FROM users WHERE email = $1`, [email])
+}
 
 const getProfilesByUserId = async userId => {
-  return query(`SELECT * FROM profiles WHERE user_id = $1`, [userId]);
-};
+  return query(`SELECT * FROM profiles WHERE user_id = $1`, [userId])
+}
 
 const getFavouritesByUserId = async userId => {
-  return query(`SELECT * FROM favourites WHERE user_id = $1`, [userId]);
-};
+  return query(`SELECT * FROM favourites WHERE user_id = $1`, [userId])
+}
 
 const updateProfileByUserId = async (userId, username, filters) => {
-  filters = JSON.stringify(filters);
+  filters = JSON.stringify(filters)
   return query(
     `UPDATE profiles SET filters = $1 WHERE user_id = $2 AND username = $3`,
     [filters, userId, username]
-  );
-};
+  )
+}
 
 const addProfileByUserId = async (userId, username, filters) => {
-  filters = JSON.stringify(filters);
+  filters = JSON.stringify(filters)
   return query(
     `INSERT INTO profiles(user_id, username, filters) values($1, $2, $3)`,
     [userId, username, filters]
-  );
-};
+  )
+}
 
 const removeProfileByUserId = async (userId, username) => {
   return query(`DELETE FROM profiles WHERE user_id = $1 AND username = $2`, [
     userId,
     username
-  ]);
-};
+  ])
+}
 
 const addFavouriteForUser = async (userId, item) => {
-  const { instagram_url_id } = item;
+  const { instagram_url_id } = item
   const inserted = await query(
     `INSERT INTO favourites(instagram_url_id, data, user_id) values($1, $2, $3) ON CONFLICT DO NOTHING RETURNING id`,
     [instagram_url_id, JSON.stringify(item), userId]
-  );
-  if (!inserted.length) return [];
-  const insertId = inserted[0].id;
-  return query(`SELECT * FROM favourites WHERE id = $1`, [insertId]);
-};
+  )
+  if (!inserted.length) return []
+  const insertId = inserted[0].id
+  return query(`SELECT * FROM favourites WHERE id = $1`, [insertId])
+}
 
 const removeFavouritesForUser = async (userId, instagramUrlId) => {
   return query(
     `DELETE FROM favourites WHERE user_id = $1 AND instagram_url_id = $2`,
     [userId, instagramUrlId]
-  );
-};
+  )
+}
 
 const createUser = async (email, password) => {
   const inserted = await query(
     `INSERT INTO users(email, password) values($1, $2) RETURNING id`,
     [email, password]
-  );
-  if (!inserted.length) return [];
-  const insertId = inserted[0].id;
-  return getUserById(insertId);
-};
+  )
+  if (!inserted.length) return []
+  const insertId = inserted[0].id
+  return getUserById(insertId)
+}
 
 module.exports = {
+  getTotalUsers,
   getUserByEmail,
   createUser,
   getProfilesByUserId,
@@ -77,4 +82,4 @@ module.exports = {
   addFavouriteForUser,
   getFavouritesByUserId,
   removeFavouritesForUser
-};
+}
